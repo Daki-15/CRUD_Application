586f2125b3445a83e780cc7e54447064abaa081c
Daki15 <darjanpaunovic@gmail.com>
2023-05-06 21:32:16 +0200

    design completion for all CRUD forms

--- design_CRUD_form.py
+++ design_CRUD_form.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 def create_form_design():
     new_window = Toplevel()
-    new_window.geometry("400x300")
+    new_window.geometry("400x400")
     new_window.title("Create")
     new_window.configure(bg="#0B314B")
     Label(new_window, text="Create operation:", anchor="n", 
@@ -19,6 +19,8 @@ def create_form_design():
     Label(new_window, text="Age:", anchor="w", 
           bg="#0B314B", fg="#F4F7F9", font=("Batang", 13)).grid(row=3, column=0, padx=5, pady=20)
     Entry(new_window, bd=2, font=("Batang", 13)).grid(row = 3, column=1)
+    # Button to create/adding data
+    Button(new_window, text="Submit", bg="#0D5F58", fg="#FEFEFE", font=("Batang", 13), width=10).grid(row=4, column=1, padx=5, pady=20)
 
 def read_form_design():
     new_window = Toplevel()
@@ -31,16 +33,42 @@ def read_form_design():
 
 def update_form_design():
     new_window = Toplevel()
-    new_window.geometry("400x200")
+    new_window.geometry("550x450")
     new_window.title("Update")
     new_window.configure(bg="#0B314B")
     Label(new_window, text="Update operation:",anchor="n", 
           bg="#0B314B", fg="#F4F7F9", font=("Batang", 15)).grid(row = 0, column=1, padx=20, pady=20)
+    # Label and entry widget where the user can input their name which they want to update/change
+    Label(new_window, text="Name which you \nwant to update/change:", anchor="w", 
+          bg="#0B314B", fg="#F4F7F9", font=("Batang", 13)).grid(row=1, column=0, padx=5, pady=20)
+    Entry(new_window, bd=2, font=("Batang", 13)).grid(row = 1, column=1)
+    # Search button
+    Button(new_window, text="Search", font=("Batang", 13), bg="#166069", fg="#FEFEFE").grid(row=1, column=3, padx=5, pady=20)
+    # Label and entry widget where the user can input their name
+    Label(new_window, text="Name:", anchor="w", 
+          bg="#0B314B", fg="#F4F7F9", font=("Batang", 13)).grid(row=2, column=0, padx=5, pady=20)
+    Entry(new_window, bd=2, font=("Batang", 13)).grid(row = 2, column=1)
+    # Label and entry widget where the user can input their last name
+    Label(new_window, bd=2, text="Last Name:", anchor="w", 
+          bg="#0B314B", fg="#F4F7F9", font=("Batang", 13)).grid(row=3, column=0, padx=5, pady=20)
+    Entry(new_window, bd=2, font=("Batang", 13)).grid(row = 3, column=1)
+    # Label and entry widget where the user can input their age
+    Label(new_window, text="Age:", anchor="w", 
+          bg="#0B314B", fg="#F4F7F9", font=("Batang", 13)).grid(row=4, column=0, padx=5, pady=20)
+    Entry(new_window, bd=2, font=("Batang", 13)).grid(row = 4, column=1)
+    # Button to update data
+    Button(new_window, text="Update", bg="#0D5F58", fg="#FEFEFE", font=("Batang", 13), width=10).grid(row=5, column=1, padx=5, pady=20)
 
 def delete_form_design():
     new_window = Toplevel()
-    new_window.geometry("400x200")
+    new_window.geometry("500x200")
     new_window.title("Delete")
     new_window.configure(bg="#0B314B")
     Label(new_window, text="Delete operation:",anchor="n", 
           bg="#0B314B", fg="#F4F7F9", font=("Batang", 15)).grid(row = 0, column=1, padx=20, pady=20)
+    # Label and entry widget where the user can input their name which they want to delete
+    Label(new_window, text="Name which you \nwant to delete:", anchor="w", 
+          bg="#0B314B", fg="#F4F7F9", font=("Batang", 13)).grid(row=1, column=0, padx=5, pady=20)
+    Entry(new_window, bd=2, font=("Batang", 13)).grid(row = 1, column=1)
+    # Button to delete data
+    Button(new_window, text="Delete", bg="#0D5F58", fg="#FEFEFE", font=("Batang", 13), width=10).grid(row=1, column=2, padx=5, pady=20)
