8e43b2bfbbecdc7db3f594b90262cc903b5a443b
Daki15 <darjanpaunovic@gmail.com>
2023-05-08 12:52:40 +0200

    adding/creating data in the database

--- crud_commands.py
+++ crud_commands.py
@@ -1,5 +1,22 @@
-def submit():
-    print("Submite")
+from pymongo import MongoClient
+
+CONNECTION_STRING = "mongodb://localhost:27017"
+DATA_BASE_NAME = "Person_DB"
+COLLECTION_NAME = "Person"
+
+def submit(name, last_name, age):
+    claster = MongoClient(CONNECTION_STRING)
+    db = claster[DATA_BASE_NAME]
+    collection = db[COLLECTION_NAME]
+
+    record = {
+        "name": name,
+        "last_name": last_name,
+        "age": age
+    }
+
+    collection.insert_one(record)
+
 
 def search():
     print("Search")
--- design_CRUD_form.py
+++ design_CRUD_form.py
@@ -6,23 +6,34 @@ def create_form_design():
     new_window.geometry("400x400")
     new_window.title("Create")
     new_window.configure(bg="#0B314B")
+    name_entry = StringVar()
+    last_name_entry = StringVar()
+    age_entry = StringVar()
+      
     Label(new_window, text="Create operation:", anchor="n", 
           bg="#0B314B", fg="#F4F7F9", font=("Batang", 15)).grid(row = 0, column=1, padx=20, pady=20)
     # Label and entry widget where the user can input their name
     Label(new_window, text="Name:", anchor="w", 
           bg="#0B314B", fg="#F4F7F9", font=("Batang", 13)).grid(row=1, column=0, padx=5, pady=20)
-    Entry(new_window, bd=2, font=("Batang", 13)).grid(row = 1, column=1)
+    Entry(new_window, bd=2, font=("Batang", 13), textvariable=name_entry).grid(row = 1, column=1)
     # Label and entry widget where the user can input their last name
     Label(new_window, bd=2, text="Last Name:", anchor="w", 
           bg="#0B314B", fg="#F4F7F9", font=("Batang", 13)).grid(row=2, column=0, padx=5, pady=20)
-    Entry(new_window, bd=2, font=("Batang", 13)).grid(row = 2, column=1)
+    Entry(new_window, bd=2, font=("Batang", 13), textvariable=last_name_entry).grid(row = 2, column=1)
     # Label and entry widget where the user can input their age
     Label(new_window, text="Age:", anchor="w", 
           bg="#0B314B", fg="#F4F7F9", font=("Batang", 13)).grid(row=3, column=0, padx=5, pady=20)
-    Entry(new_window, bd=2, font=("Batang", 13)).grid(row = 3, column=1)
+    Entry(new_window, bd=2, font=("Batang", 13), textvariable=age_entry).grid(row = 3, column=1)
+
+    # define function to handle button click
+    def handle_submit():
+        name = name_entry.get()
+        last_name = last_name_entry.get()
+        age = age_entry.get()
+        submit(name, last_name, age)
     # Button to create/adding data
     Button(new_window, text="Submit", bg="#0D5F58", fg="#FEFEFE", 
-           font=("Batang", 13), width=10, command=submit).grid(row=4, column=1, padx=5, pady=20)
+           font=("Batang", 13), width=10, command=handle_submit).grid(row=4, column=1, padx=5, pady=20)
 
 def read_form_design():
     new_window = Toplevel()
